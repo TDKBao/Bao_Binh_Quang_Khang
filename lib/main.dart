b90e152e57c965f2ae46e2e63c76260c0316943a
Kim Bao <bao.tdk2369@gmail.com>
2021-07-21 21:36:57 +0700

    create dev branch
    This branch is using for merge code team and reviewing.

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'src/app.dart';
+
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: App(),
-    );
-  }
-}
-
-class App extends StatefulWidget {
-  App({Key? key}) : super(key: key);
-
-  @override
-  _AppState createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text('Hello world!')),
-    );
-  }
+  runApp(App());
 }

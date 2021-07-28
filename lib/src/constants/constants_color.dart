143fa23f58d73b42b9decf5d97acd626949c4826
Kim Bao <bao.tdk2369@gmail.com>
2021-07-28 11:05:52 +0700

    constant text and color , finish tablet ui forgot password

--- lib/src/constants/constants_color.dart
+++ lib/src/constants/constants_color.dart
@@ -3,3 +3,9 @@ import 'package:flutter/material.dart';
 class SignUp {
   static const title = Color(0xff030F09);
 }
+
+class ForgotPasswordColor {
+  static const titleForgotColor = Color(0xFF030F09);
+  static const subTitleForgotColor = Color(0xffA8A8A8);
+  static const underLineColor = Color(0xff30BE76);
+}
--- lib/src/screens/forgotpassword_screen.dart
+++ lib/src/screens/forgotpassword_screen.dart
@@ -1,89 +1,202 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:mobile_app/src/constants/constants_color.dart';
 
 import '../widgets/logo.dart';
 import '../constants/constants_text.dart';
+import '../widgets/responsive.dart';
 
 class ForgotPasswordScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 25),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 65,
-              ),
-              Logo(
-                onboardingScreen: false,
-                tabletScreen: false,
-              ),
-              SizedBox(
-                height: 41,
-              ),
-              Text(ForgotPassword.titleResetPassword,
-                  style: Theme.of(context)
-                      .textTheme
-                      .headline4
-                      ?.copyWith(fontSize: 32, color: Color(0xff030F09))),
-              SizedBox(
-                height: 29.12,
-              ),
-              Text(ForgotPassword.subtitleResetPassword,
-                  style: Theme.of(context)
-                      .textTheme
-                      .subtitle1
-                      ?.copyWith(fontSize: 15)),
-              Spacer(),
-              Text(
-                ForgotPassword.emailAddress,
-                style: Theme.of(context).textTheme.subtitle1!.copyWith(
-                    fontFamily: 'Nunito',
-                    color: Color(0xffA8A8A8),
-                    fontSize: 15),
-              ),
-              TextField(
-                keyboardType: TextInputType.emailAddress,
-                autofocus: true,
-                cursorColor: Color(0xff30BE76),
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.only(top: 15, bottom: 5),
-                  isDense: true,
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Color(0xff30BE76),
-                      width: 2,
-                    ),
+        child: Responsive(
+            mobile: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 65,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25),
+                  child: Logo(
+                    onboardingScreen: false,
+                    tabletScreen: false,
                   ),
                 ),
-                style: Theme.of(context).textTheme.bodyText1!.copyWith(
-                    fontFamily: 'Nunito',
-                    color: Color(0xff030F09),
-                    fontSize: 16.5),
-              ),
-              SizedBox(
-                height: 53,
-              ),
-              TextButton(
-                  style: TextButton.styleFrom(
-                      backgroundColor: Color(0xff30BE76),
-                      minimumSize: Size(MediaQuery.of(context).size.width, 50),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8))),
-                  onPressed: () {},
-                  child: Text(
-                    ForgotPassword.buttonSend,
-                    style: Theme.of(context)
-                        .textTheme
-                        .headline5
-                        ?.copyWith(color: Colors.white),
-                  )),
-              Spacer(),
-            ],
+                SizedBox(
+                  height: 41,
+                ),
+                BoxResetPassword(),
+              ],
+            ),
+            tablet: Stack(
+              children: [
+                Opacity(
+                  opacity: 0.7,
+                  child: Image.asset(
+                    'assets/images/login_background.jpeg',
+                    fit: BoxFit.cover,
+                    width: double.infinity,
+                    height: double.infinity,
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          Colors.white.withOpacity(0),
+                          Colors.white.withOpacity(0.5),
+                          Colors.white.withOpacity(0.7),
+                          Colors.white.withOpacity(1),
+                        ]),
+                  ),
+                ),
+                Center(
+                    child: Column(
+                  children: [
+                    Spacer(),
+                    Logo(
+                      onboardingScreen: false,
+                      tabletScreen: true,
+                    ),
+                    Spacer(),
+                    Text(
+                      ForgotPassword.titleResetPassword,
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline5
+                          ?.copyWith(fontFamily: AppConstants.fontRegular),
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(Radius.circular(8)),
+                      ),
+                      height: 547,
+                      width: MediaQuery.of(context).size.width / 2,
+                      child: BoxResetPassword(),
+                    ),
+                    Spacer(
+                      flex: 2,
+                    )
+                  ],
+                ))
+              ],
+            )),
+      ),
+    );
+  }
+}
+
+class BoxResetPassword extends StatelessWidget {
+  const BoxResetPassword({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final auth = FirebaseAuth.instance;
+    final _email = TextEditingController();
+    double sizeTitleResetPassword;
+    double spacingHeadTitle;
+    double spacingSubTitle;
+    double spacingTextBox;
+    double spacingButton;
+    double widthSizeScreen = MediaQuery.of(context).size.width;
+    double paddingScreen = 25;
+    if (widthSizeScreen >= 650) {
+      sizeTitleResetPassword = 40;
+      spacingHeadTitle = 33;
+      spacingSubTitle = 83;
+      spacingTextBox = 100;
+      spacingButton = 27.58;
+    } else {
+      sizeTitleResetPassword = 32;
+      spacingHeadTitle = 0;
+      spacingSubTitle = 23;
+      spacingTextBox = 138;
+      spacingButton = 53;
+    }
+
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: paddingScreen),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: spacingHeadTitle,
           ),
-        ),
+          Text(ForgotPassword.titleResetPassword,
+              style: Theme.of(context).textTheme.headline4?.copyWith(
+                  fontSize: sizeTitleResetPassword,
+                  color: ForgotPasswordColor.titleForgotColor)),
+          SizedBox(
+            height: spacingSubTitle,
+          ),
+          Text(ForgotPassword.subtitleResetPassword,
+              style: Theme.of(context)
+                  .textTheme
+                  .subtitle1
+                  ?.copyWith(fontSize: 15)),
+          SizedBox(
+            height: spacingTextBox,
+          ),
+          // Spacer(),
+          Text(
+            ForgotPassword.emailAddress,
+            style: Theme.of(context).textTheme.subtitle1!.copyWith(
+                fontFamily: AppConstants.fontRegular,
+                fontWeight: FontWeight.normal,
+                color: ForgotPasswordColor.subTitleForgotColor,
+                fontSize: 15),
+          ),
+          TextField(
+            controller: _email,
+            keyboardType: TextInputType.emailAddress,
+            autofocus: true,
+            cursorColor: ForgotPasswordColor.underLineColor,
+            decoration: InputDecoration(
+              contentPadding: EdgeInsets.only(top: 15, bottom: 5),
+              isDense: true,
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: ForgotPasswordColor.underLineColor,
+                  width: 2,
+                ),
+              ),
+            ),
+            style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                fontFamily: AppConstants.fontRegular,
+                color: ForgotPasswordColor.subTitleForgotColor,
+                fontSize: 16.5),
+          ),
+          SizedBox(
+            height: spacingButton,
+          ),
+          TextButton(
+              style: TextButton.styleFrom(
+                  backgroundColor: ForgotPasswordColor.underLineColor,
+                  minimumSize: Size(widthSizeScreen, 50),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8))),
+              onPressed: () {
+                auth.sendPasswordResetEmail(email: _email.text);
+              },
+              child: Text(
+                ForgotPassword.buttonSend,
+                style: Theme.of(context)
+                    .textTheme
+                    .headline5
+                    ?.copyWith(color: Colors.white),
+              )),
+        ],
       ),
     );
   }
